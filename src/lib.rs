86d1ded59135dbb3fe25c79cc3bf4d8ee393c76c
TehPers <tehperz@gmail.com>
2024-11-26 00:53:41 -0800

    Move lints to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -73,21 +73,6 @@
 //!   [regex](https://crates.io/crates/regex) to execute them.
 //! - `colors`*: Enables styled failure messages. Styled messages can always be
 //!   disabled by setting `NO_COLOR`.
-#![warn(
-    missing_debug_implementations,
-    missing_docs,
-    trivial_casts,
-    trivial_numeric_casts,
-    unused_extern_crates,
-    unused_import_braces,
-    unused_qualifications,
-    unused_results,
-    clippy::all,
-    clippy::pedantic,
-    clippy::style
-)]
-#![allow(clippy::missing_errors_doc, clippy::module_name_repetitions)]
-#![forbid(unsafe_code)]
 
 pub mod assertions;
 pub mod metadata;
